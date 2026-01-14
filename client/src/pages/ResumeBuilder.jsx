16254b56ac01eaf570dcc8d99191abd57a9ec59a
Subhodeep77 <subhodeepparamanik22413@gmail.com>
2026-01-15 01:09:35 +0530

    Frontend update

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -6,9 +6,13 @@ import {
   Briefcase,
   ChevronLeft,
   ChevronRight,
+  DownloadIcon,
+  EyeIcon,
+  EyeOffIcon,
   FileText,
   FolderIcon,
   GraduationCap,
+  Share2Icon,
   Sparkles,
   User,
 } from "lucide-react";
@@ -20,6 +24,7 @@ import ProfessionalSummary from "../components/ProfessionalSummary";
 import ExperienceForm from "../components/ExperienceForm";
 import EducationForm from "../components/EducationForm";
 import ProjectForm from "../components/ProjectForm";
+import SkillsForm from "../components/SkillsForm";
 const ResumeBuilder = () => {
   const { resumeId } = useParams();
   const [resumeData, setResumeData] = useState({
@@ -62,6 +67,26 @@ const ResumeBuilder = () => {
     loadExistingResume();
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const changeResumeVisibility = async () => {
+    setResumeData({...resumeData, public: !resumeData.public})
+  }
+
+  const handleShare = () => {
+    const frontendUrl = window.location.href.split('/app')[0]
+    const resumeUrl  = frontendUrl + '/view' + resumeId
+
+    if(navigator.share){
+      navigator.share({url: resumeUrl, text: "My Resume"})
+    }
+    else{
+      alert('Share not supported on this browser.')
+    }
+  }
+
+  const downloadResume = () => {
+    window.print()
+  }
 
   return (
     <div>
@@ -163,12 +188,34 @@ const ResumeBuilder = () => {
                 {activeSection.id === 'projects' && (
                   <ProjectForm data={resumeData.project} onChange={(data) => setResumeData(prev => ({...prev, project:data}))}/>
                 )}
+                {activeSection.id === 'skills' && (
+                  <SkillsForm data={resumeData.skills} onChange={(data) => setResumeData(prev => ({...prev, skills:data}))}/>
+                )}
               </div>
+              <button className="bg-linear-to-br from-indigo-100 to-indigo-200 ring-indigo-300 text-indigo-600 ring hover:ring-indigo-400 transition-all rounded-md px-6 py-2 mt-6 text-sm">Save Changes</button>
             </div>
           </div>
           {/* Right Panel - Preview */}
           <div className="lg:col-span-7 max-lg:mt-6">
-            <div>{/* buttons */}</div>
+            <div className="relative w-full">
+              <div className="absolute bottom-3 left-0 right-0 flex items-center justify-end gap-2">
+                {resumeData.public && (
+                  <button onClick={handleShare} className="flex items-center p-2 px-4 gap-2 text-xs bg-linear-to-br from-blue-100 to-blue-200 text-blue-600 rounded-lg ring-blue-300 hover:ring transition-colors">
+                    <Share2Icon className="size-4"/> Share
+                  </button>
+                )}
+                <button onClick={changeResumeVisibility} className="flex items-center p-2 px-4 gap-2 text-xs bg-linear-to-br from-purple-100 to-purple-200 text-purple-600 ring-purple-300 rounded-lg hover:ring transition-colors">{
+                  resumeData.public ? <EyeIcon className="size-4"/> : <EyeOffIcon className="size-4"/>
+                }
+                {
+                  resumeData.public ? 'Public' : 'Private'
+                }
+                </button>
+                <button onClick={downloadResume} className="flex items-center px-6 py-2 gap-2 text-xs bg-linear-to-br from-purple-100 to-purple-200 text-purple-600 ring-purple-300 rounded-lg hover:ring transition-colors">
+                  <DownloadIcon className="size-4"/>Download
+                </button>
+              </div>
+            </div>
             <ResumePreview
               data={resumeData}
               template={resumeData.template}
