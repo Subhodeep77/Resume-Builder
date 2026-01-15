5b8eb5295ade9770d5f21ff8384113e84a9e0b7b
Subhodeep77 <subhodeepparamanik22413@gmail.com>
2026-01-15 19:43:10 +0530

    Backend update

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/db.js'
 import userRouter from './routes/userRoutes.js'
+import resumeRouter from './routes/resumeRouter.js'
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -14,6 +15,7 @@ await connectDB()
 
 app.get('/',(req,res) => res.send('Server is live...'))
 app.use('/api/users', userRouter)
+app.use('/api/resumes', resumeRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
