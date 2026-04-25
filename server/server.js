88133ba6939d73cf5f6dcb9f6ab17a9666d49a04
Subhodeep77 <subhodeepparamanik22413@gmail.com>
2026-04-25 14:47:26 +0530

    frontend+backend

--- server/server.js
+++ server/server.js
@@ -10,7 +10,12 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
-app.use(cors())
+
+app.use(cors({
+  origin: ["https://1resumebuilder.netlify.app","http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 await connectDB()
 
